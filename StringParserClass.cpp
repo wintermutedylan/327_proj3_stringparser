811df167228640c4e4a5a38028c60aac1a46375b
Dylan Wintermute <dylan.wintermute@cnu.edu>
2019-10-31 17:37:23 -0400

    score = 75 working on find tag and get data between tags

--- StringParserClass.cpp
+++ StringParserClass.cpp
@@ -7,6 +7,77 @@
 
 #include <string>
 #include <string.h>
+#include <iostream>
+#include "../327_proj3_test/includes/constants.h"
+#include "../327_proj3_test/includes/StringParserClass.h"
+
+using namespace KP_StringParserClass;
+StringParserClass::StringParserClass(void){
+	this->pStartTag = NULL;
+	this->pEndTag = NULL;
+	this->areTagsSet = false;
+}
+StringParserClass::~StringParserClass(){
+	cleanup();
+}
+
+int StringParserClass::setTags(const char *pStart, const char *pEnd){
+	if (pStart == NULL || pEnd == NULL){
+		return ERROR_TAGS_NULL;
+	}
+	else {
+		int lenS = strlen(pStart);
+		pStartTag = new char[lenS + 1];
+		int lenE = strlen(pEnd);
+		pEndTag = new char[lenE + 1];
+
+		strncpy(pStartTag, pStart, lenS);
+		strncpy(pEndTag, pEnd, lenE);
+
+		return SUCCESS;
+	}
+
+
+}
+
+
+int StringParserClass::getDataBetweenTags(char *pDataToSearchThru, std::vector<std::string> &myVector){
+	if (pStartTag == NULL || pEndTag == NULL){
+		return ERROR_TAGS_NULL;
+	}
+	if (pDataToSearchThru == NULL){
+		return ERROR_DATA_NULL;
+	}
+	myVector.clear();
+	std::string tagsToFind(pDataToSearchThru);
+
+
+
+	//findTag(pStartTag, pDataToSearchThru)
+	return SUCCESS;
+
+
+}
+void StringParserClass::cleanup(){
+	if (pStartTag){
+		delete[] pStartTag;
+	}
+	if (pEndTag){
+		delete[] pEndTag;
+	}
+}
+
+int StringParserClass::findTag(char *pTagToLookFor, char *&pStart, char *&pEnd){
+	if (pStart == NULL || pEnd == NULL){
+		return ERROR_TAGS_NULL;
+	}
+	std::string tagsToFind(pTagToLookFor);
+
+
+	return SUCCESS;
+
+}
+
 
 //TODO Fill in
 
